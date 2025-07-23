d251c5601dd71bda96e7ce0ff8254b8d20ff8dc4
Abhinav022569 <abhinav911@gmail.com>
2025-07-23 22:12:33 +0530

    yeah i did that

--- users/dashboard/profile/profile_page.php
+++ users/dashboard/profile/profile_page.php
@@ -236,7 +236,7 @@ if ($conn && !$conn->connect_error) {
           <li><a href="../notes/notes.php"><i class="fas fa-book"></i> Notes</a></li>
           <li><a href="../chat_box/group_chat.php"><i class="fas fa-comments"></i> Group Chat</a></li>
           <li><a href="../to-do/to-do.php"><i class="fas fa-clipboard-list"></i> To-Do List</a></li>
-          <li class="active"><a href="profile__page.php"><i class="fas fa-user-circle"></i> Profile</a></li>
+          <li class="active"><a href="profile_page.php"><i class="fas fa-user-circle"></i> Profile</a></li>
         </ul>
       </nav>
       <div class="sidebar-footer">
